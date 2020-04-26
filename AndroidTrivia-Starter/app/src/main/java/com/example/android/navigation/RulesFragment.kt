5f74f9a11dfefb1ab5ef88b97c0eaf46f7fd3d36
Saketh-Karumuri <saketh.ram18@gmail.com>
2020-04-26 12:08:20 -0500

    Code lab lesson 3 activity

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/RulesFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/RulesFragment.kt
@@ -21,11 +21,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.android.navigation.databinding.FragmentRulesBinding
 
 class RulesFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rules, container, false)
+        val binding: FragmentRulesBinding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_rules, container, false)
+        binding.playButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(
+                    RulesFragmentDirections.actionRulesFragmentToGameFragment())
+        }
+        return binding.root
     }
 }
---
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,47 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_title, container, false)
+        binding.rulesButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(
+                    TitleFragmentDirections.actionTitleFragmentToRulesFragment())
+        }
+        binding.playButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(
+                    TitleFragmentDirections.actionTitleFragmentToGameFragment())
+        }
+        binding.aboutButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(
+                    TitleFragmentDirections.actionTitleFragmentToAboutFragment())
+        }
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+                view!!.findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
+}
